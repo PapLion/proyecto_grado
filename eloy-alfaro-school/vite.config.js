2158084da5dedfcd89140cbd1bb62acd133296e6
ExperienceV <132963842+ExperienceV@users.noreply.github.com>
2025-02-06 04:14:29 +0000

    xd

--- eloy-alfaro-school/vite.config.js
+++ eloy-alfaro-school/vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { createHtmlPlugin } from 'vite-plugin-html'
 import { resolve } from 'path'
 
 export default defineConfig({
@@ -14,38 +13,6 @@ export default defineConfig({
       }
     }
   },
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-      pages: [
-        {
-          entry: '/main.js',
-          filename: 'index.html',
-          template: 'index.html',
-        },
-        {
-          entry: '/main.js',
-          filename: 'pages/historia.html',
-          template: 'pages/historia.html',
-        },
-        {
-          entry: '/main.js',
-          filename: 'pages/contacto.html',
-          template: 'pages/contacto.html',
-        },
-        {
-          entry: '/main.js',
-          filename: 'pages/oferta-academica.html',
-          template: 'pages/oferta-academica.html',
-        },
-        {
-          entry: '/main.js',
-          filename: 'pages/actividades.html',
-          template: 'pages/actividades.html',
-        },
-      ],
-    }),
-  ],
   root: './',
   base: '/'
 })
